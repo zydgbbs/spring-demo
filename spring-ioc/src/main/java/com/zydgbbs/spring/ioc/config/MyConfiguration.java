8182e8489e7a7eaf51c89492290de1fefa75760f
zydgbbs <admin@zydgbbs.com>
2019-12-08 09:51:11 +0800

    可以添加扫描注解

--- spring-ioc/src/main/java/com/zydgbbs/spring/ioc/config/MyConfiguration.java
+++ spring-ioc/src/main/java/com/zydgbbs/spring/ioc/config/MyConfiguration.java
@@ -2,9 +2,11 @@ package com.zydgbbs.spring.ioc.config;
 
 import com.zydgbbs.spring.ioc.domain.User;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+//@ComponentScan("com.zydgbbs.spring.ioc")
 public class MyConfiguration {
     @Bean
     public User user(){
