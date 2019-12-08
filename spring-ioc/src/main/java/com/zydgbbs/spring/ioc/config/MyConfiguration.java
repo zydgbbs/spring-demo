254851d34bf39cb590ba8a203e833b1e76e3c672
zydgbbs <admin@zydgbbs.com>
2019-12-08 09:58:49 +0800

    Configuration导入其他配置类

--- spring-ioc/src/main/java/com/zydgbbs/spring/ioc/config/MyConfiguration.java
+++ spring-ioc/src/main/java/com/zydgbbs/spring/ioc/config/MyConfiguration.java
@@ -4,9 +4,11 @@ import com.zydgbbs.spring.ioc.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-//@ComponentScan("com.zydgbbs.spring.ioc")
+//@ComponentScan("com.zydgbbs.spring.ioc")扫描包
+//@Import(OtherConfiguration.class)导入其他配置类
 public class MyConfiguration {
     @Bean
     public User user(){
---
+++ spring-ioc/src/main/java/com/zydgbbs/spring/ioc/config/OtherConfiguration.java
@@ -0,0 +1,7 @@
+package com.zydgbbs.spring.ioc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OtherConfiguration {
+}
